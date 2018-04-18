a980fdc3c793d66acfd42ead13710f8703c79323
congson0710 <congson0710@gmail.com>
2018-04-18 20:35:44 +0700

    apply Redux

---
+++ IFINITY TEAM/client/src/actions/action.type.js
@@ -0,0 +1,1 @@
+// action type definition goes here
--- IFINITY TEAM/client/src/index.js
+++ IFINITY TEAM/client/src/index.js
@@ -1,21 +1,37 @@
 import { AppContainer } from 'react-hot-loader';
 import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
 import React from 'react';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import reduxThunk from 'redux-thunk';
+import reduxLogger from 'redux-logger';
 import ReactDOM from 'react-dom';
 
 import MainApp from './containers/app';
+import reducers from './reducers';
+
+const store = createStore(
+  reducers,
+  {},
+  applyMiddleware(reduxThunk, reduxLogger)
+);
 
 const render = Component =>
   ReactDOM.render(
-    <AppContainer>
-      <Router>
-        <Component />
-      </Router> 
-    </AppContainer>,
+    <Provider store={store}>
+      <AppContainer>
+        <Router>
+          <Component />
+        </Router>
+      </AppContainer>
+    </Provider>,
     document.getElementById('root')
   );
 
 render(MainApp);
 
 // Webpack Hot Module Replacement API
-if (module.hot) module.hot.accept('./containers/app', () => render(require('./containers/app').default));
+if (module.hot)
+  module.hot.accept('./containers/app', () =>
+    render(require('./containers/app').default)
+  );
