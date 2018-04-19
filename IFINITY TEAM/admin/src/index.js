e47d689244fcd39f4a9aa16a070de4539b0d11aa
quyenphamkhac <phamkhacquyen1996@gmail.com>
2018-04-19 09:54:28 +0700

    update antd for infinity admin

--- IFINITY TEAM/admin/src/index.js
+++ IFINITY TEAM/admin/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './container/App';
+import 'antd/dist/antd.css'; 
 
 import { BrowserRouter as Router } from 'react-router-dom';
 
