69c5cc60072e2960ed7598ad3c97d2de3adb48e2
Minh Quang <minhquangnguyen1996@gmail.com>
2018-04-23 15:16:16 +0700

    edit Home.js

--- IFINITY TEAM/client/src/screens/app/components/Home/Home.js
+++ IFINITY TEAM/client/src/screens/app/components/Home/Home.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import Slide from './Slide';
+import SaleFood from './SaleFood';
 class Home extends Component {
     render() {
         return(
-            <div style={{ minHeight: 500}}>
-                
+            <div class="container" style={{ minHeight: 500}}>
+                <Slide/>
+                <hr class="hr-text" data-content="Món ngon giảm giá"/>
+                <SaleFood/>
+                <hr class="hr-text" data-content="Đặt hàng nhiều trong tuần"/>
             </div>
         )
     }
