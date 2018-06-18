ba290a37f82ed7f4a62676f09aeade7407505659
quyenphamkhac <pkquyen@sk-global.biz>
2018-06-18 16:42:58 +0700

    Finish Shopping Cart Function

--- FINAL_PROJECT/routes/cart.js
+++ FINAL_PROJECT/routes/cart.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var Product = require('../models/productModel');
+var Order = require('../models/orderModel');
+
 var cartRepo = require('../repos/cartRepo');
+
+var moment = require('moment');
 
 router.post('/add', function(req, res, next) {
     Product.findOne({ productId: req.body.proId }, function(err, product) {
@@ -42,6 +46,60 @@ router.post('/reduce', function(req, res, next) {
 router.post('/remove', function(req, res, next) {
     cartRepo.remove(req.session.cart, req.body.proId);
     res.redirect(req.headers.referer);
+});
+
+router.post('/checkout', function(req, res, next) {
+    var products = req.session.cart.map((item) => {
+        return {
+            product: item.product._id,
+            quantity: item.quantity,
+            price: item.price
+        }
+    });
+    var user = req.session.currentUser._id;
+    var totalPrice = cartRepo.getTotalPrice(req.session.cart);
+    
+    var bale = {
+        user: user,
+        products: products,
+        totalPrice: totalPrice,
+        status: 0,
+    }
+
+    var vm = {
+        layout: "home.handlebars",
+    }
+
+    var newId = null;
+    bale.time = moment().format("DD/MM/YYYY, hh:mm:ss A");
+    bale.createdAt = new Date();
+    Order.findOne().sort({createdAt: -1}).exec(function(err, lastDto) {
+        if(err) {
+            console.log("[Error]", err);
+            vm.showError = true;
+            vm.errorMsg = "Thanh toán thất bại";
+            res.render('_gio_hang/gio_hang', vm);
+        } else {
+            newId = lastDto === null ? 1 : parseInt(lastDto.orderId, 10) + 1;
+            bale.orderId = newId;
+            var order = new Order(bale);
+            order.save(function(err, orderDto) {
+                if(err) {
+                    vm.showError = true;
+                    vm.errorMsg = "Thanh toán thất bại";
+                    res.render('_gio_hang/gio_hang', vm);
+                }
+                else {
+                    //clear cart
+                    req.session.cart = [];
+                    
+                    vm.showResult = true;
+                    vm.resultMsg = "Thanh toán thành công";
+                    res.render('_gio_hang/gio_hang', vm);
+                }
+            });
+        }
+    });
 });
 
 module.exports = router;
