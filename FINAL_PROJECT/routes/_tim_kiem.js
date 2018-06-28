ae772ce13c9e03cf491d01ec6f94e987ffb7ebb8
congson0710 <congson0710@gmail.com>
2018-06-28 14:07:44 +0700

    implement Search feature

--- FINAL_PROJECT/routes/_tim_kiem.js
+++ FINAL_PROJECT/routes/_tim_kiem.js
@@ -1,69 +1,105 @@
 var express = require('express');
 var router = express.Router();
+const _ = require('lodash')
 
 var Brand = require('../models/brandModel');
 var Product = require('../models/productModel');
 var Category = require('../models/categoryModel');
 
 /* GET brand list page */
-router.get('/nhan_hieu', function(req, res, next) {
+router.get('/nhan_hieu', function (req, res, next) {
     var id = req.query.id;
-    if(id === undefined) {
+    if (id === undefined) {
         return res.redirect('/home');
     }
 
     var vm = {
-        layout: "home.handlebars"
-    }
+        layout: 'home.handlebars',
+    };
 
-    Brand.findOne({ brandId: id }, function(err, brand) {
-        if(err) return res.redirect('/home');
-        else {
-            if(!brand) {
-                vm.products = []
-                return res.render('_nhan_hieu/nhan_hieu', vm);
-            }
-
-            Product.find({ brand: brand._id}, function(err, productDtos) {
-                if(err) return res.redirect('/home');
-                else {
-                    vm.products = productDtos;
-                    vm.brandName = brand.brandName;
+    Brand.findOne({
+            brandId: id,
+        },
+        function (err, brand) {
+            if (err) return res.redirect('/home');
+            else {
+                if (!brand) {
+                    vm.products = [];
                     return res.render('_nhan_hieu/nhan_hieu', vm);
-                } 
-            });
+                }
+
+                Product.find({
+                        brand: brand._id,
+                    },
+                    function (err, productDtos) {
+                        if (err) return res.redirect('/home');
+                        else {
+                            vm.products = productDtos;
+                            vm.brandName = brand.brandName;
+                            return res.render('_nhan_hieu/nhan_hieu', vm);
+                        }
+                    }
+                );
+            }
         }
-    });
+    );
 });
 
-router.get('/loai_ruou', function(req, res, next) {
+router.get('/loai_ruou', function (req, res, next) {
     var id = req.query.id;
-    if(id === undefined) {
+    if (id === undefined) {
         return res.redirect('/home');
     }
 
     var vm = {
-        layout: "home.handlebars"
-    }
+        layout: 'home.handlebars',
+    };
 
-    Category.findOne({ categoryId: id }, function(err, cat) {
-        if(err) return res.redirect('/home');
-        else {
-            if(!cat) {
-                vm.products = []
-                return res.render('_loai_ruou/loai_ruou', vm);
-            }
-
-            Product.find({ category: cat._id}, function(err, productDtos) {
-                if(err) return res.redirect('/home');
-                else {
-                    vm.products = productDtos;
-                    vm.catName = cat.categoryName;
+    Category.findOne({
+            categoryId: id,
+        },
+        function (err, cat) {
+            if (err) return res.redirect('/home');
+            else {
+                if (!cat) {
+                    vm.products = [];
                     return res.render('_loai_ruou/loai_ruou', vm);
-                } 
-            });
+                }
+
+                Product.find({
+                        category: cat._id,
+                    },
+                    function (err, productDtos) {
+                        if (err) return res.redirect('/home');
+                        else {
+                            vm.products = productDtos;
+                            vm.catName = cat.categoryName;
+                            return res.render('_loai_ruou/loai_ruou', vm);
+                        }
+                    }
+                );
+            }
         }
-    });
+    );
+});
+
+router.post('/tim_kiem', async (req, res, next) => {
+    const search = req.body.search
+
+    const vm = {
+        layout: 'home.handlebars',
+    };
+
+    const re = new RegExp(_.escapeRegExp(search), 'i');
+
+    const listProd = await Product.find({})
+
+    vm.products = listProd.filter(product => {
+        if (re.test(product.productName))
+            return product
+    })
+
+    res.render('_tim_kiem/tim_kiem', vm)
 });
 
 module.exports = router;
