43d703f5cbe42432d3c890fd93c31ecf2e8dc37b
congson0710 <congson0710@gmail.com>
2018-06-28 17:05:12 +0700

    implement Product Detail feature

--- FINAL_PROJECT/controllers/sanPhamController.js
+++ FINAL_PROJECT/controllers/sanPhamController.js
@@ -1,15 +1,40 @@
 var express = require('express');
 var router = express.Router();
 var Product = require('../models/productModel');
+const Brand = require('../models/brandModel')
+const Category = require('../models/categoryModel')
 
 var sanPhamController = {};
 
-sanPhamController.render = (req, res, next) => {
+sanPhamController.render = async (req, res, next) => {
     var vm = {
         layout: "home.handlebars"
     }
-    var idProduct = req.query.id;
-    console.log(idProduct);
+    var id = req.query.id;
+
+    const result = await Product.findOne({
+        productId: id
+    })
+
+    const brand = await Brand.findOne({
+        _id: result.brand
+    })
+
+    const category = await Category.findOne({
+        _id: result.category
+    })
+
+    vm.product = {
+        productName: result.productName,
+        view: result.view,
+        purchases: result.purchases,
+        price: result.price,
+        description: result.description,
+        brand: brand.brandName,
+        category: category.categoryName,
+        thumbnail: result.thumbnail
+    }
+
     res.render('_san_pham/san_pham', vm);
 }
 
